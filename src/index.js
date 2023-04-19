61204dcb3d2ce8e39f4d8409588053d84d0a3144
Elisabeth Garcia <lisgarciae@gmail.com>
2023-04-19 16:01:18 -0400

    stop point

--- src/index.js
+++ src/index.js
@@ -1,23 +1,43 @@
 // write your code herec
-const ramenList=document.querySelector("#ramen-menu")
 let ramenMenu 
 let ramenArry 
 let currentRamen
+const ramenList=document.querySelector("#ramen-menu")
+const imgDetail = document.querySelector(".detail-image")
+const name1 = document.querySelector(".name")
+const restaurant = document.querySelector(".restaurant")
+const rating = document.querySelector("#rating-display")
+const comment=document.querySelector("#comment-display")
+
 // 1. See all ramen images in the div with the id of ramen-menu. 
         //a. When the page loads, request the data from the server to get all the ramen objects. 
         //b Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
 
+        //my code
 function getRamen () {
     return fetch("http://localhost:3000/ramens")
+    .catch(err => console.log(err))
         .then(resp => resp.json())  //endpoint
         .then (ramens => {
             ramenArry=ramens
-
-            ramenArry.map(ramens=> {
+            currentRamen=ramenArry[0]
+            ramenArry.map(ramens=> {     //iterate through data
                 addImageToDiv(ramens)})})
     ramenDetail(currentRamen)
         }
-
+        // //for each iteration RK....didn't work for me with different tags, map is okay
+        // function getRamen () {
+        //     return fetch("http://localhost:3000/ramens")
+        //     .catch(err => console.log(err))
+        //         .then(resp => resp.json())  //endpoint
+        //         .then (ramens => {
+        //             ramenArry=ramens
+        //             currentRamen=ramenArry[0]
+        //             ramenArry.forEach(currentRamen=> {     //iterate through data
+        //                 addImageToDiv(ramens)})})
+        //     ramenDetail(currentRamen)
+        //         }
+        
 
 
 function addImageToDiv (ramens) {
@@ -32,12 +52,6 @@ function addImageToDiv (ramens) {
 // 2. Click on an image from the #ramen-menu div and see all the info about that ramen displayed inside the #ramen-detail div
         //  and where it says insert comment here and insert rating here.  
 
-const imgDetail = document.querySelector(".detail-image")
-const name1 = document.querySelector(".name")
-const restaurant = document.querySelector(".restaurant")
-const rating = document.querySelector("#rating-display")
-const comment=document.querySelector("#comment-display")
-
 function ramenDetail (ramens) {
    imgDetail.src = ramens.image
    name1.textContent = ramens.name
@@ -50,20 +64,30 @@ function ramenDetail (ramens) {
 const ramenForm=document.querySelector("#new-ramen") //selects input form-working
 
 
-// const newRamenInfo = ("change", (e)) => {
-//     name1: e.target.name.value
-//     restaurant: e.target.restaurant.value
-//     imgDetail: e.target.image.value
-//     rating: e.target.rating.value
-//     comment: e.target.comment.value
+function getNewRamenInfo (newRamen) {
+    ramenForm.addEventListener("click", (e) => {   //changed from change to submit
+    e.preventDefault()
+    name1.textContent=  e.target.name.value
+    restaurant.textContent = e.target.restaurant.value
+    imgDetail.src= e.target.image.value
+    rating.textContent=e.target.rating.value
+    e.target.comment.value
 
-//     ramenList.append(newRamenInfo)
+    ramenList.append(newRamen)
+    
+})}
+console.log(ramenList)
+
+// function newRamenForm(ramens) {
+//     const 
 // }
 
-// function submitForm(ramenForm) {
-//     ramenForm.addEventListener("submit", (e)=> 
-//     {e.preventDefault()})
-// }
+function submitForm(ramenForm) {
+    ramenForm.addEventListener("submit", (e)=> {
+        e.preventDefault()
+        
+    })
+}
 
 
 
